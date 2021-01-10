9902973a2e942557ee734c00d439e682dc6b6d05
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-01-10 05:38:34 -0800

    Add files via upload

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import './header.css'
 
+function Header() {
+    return (
+        <>
+            <div className='row border-bottom'>
+                <div className='col-md-4'>
+                    <img src='http://ttdc.in/DeptLogin/BoatDepartment/images/TTDCLogo.svg' alt='ttdc-logo' style={{ height: '80px', width: "460px" }}></img>
+                </div>
+                <div className='col-md-3 d-flex align-items-center justify-content-center'>
+                    <span className='sp-2'>Welcome User..!</span>
+                </div>
+                <div className='col-md-5 d-flex align-items-center justify-content-center'>
+                    <span className='sp-1'>USER PORTAL</span>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Header
